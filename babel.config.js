e4658d5d58c678e2079c71094e126ad8786eb838
Austin Sullivan <ausulliv@redhat.com>
2023-03-28 15:16:14 -0400

    fix(tests): Fix config issues causing test failures

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,10 @@ module.exports = {
     ['@babel/preset-env', { targets: { node: 'current' } }],
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-flow',
-    '@babel/preset-typescript'
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-proposal-class-properties',
   ]
 };
