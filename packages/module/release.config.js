19d5289da4beb1ab152bcab90f364413f3789d83
Jenny <32821331+jenny-s51@users.noreply.github.com>
2023-08-10 14:29:27 -0400

    fix: v5 updates

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   branches: [
     'do-not-delete',
-    { name: 'main', channel: 'prerelease-v4', range: '4.x' },
-    { name: 'v5', channel: 'prerelease', prerelease: 'prerelease' }
+    { name: 'v4', channel: 'prerelease-v4', range: '4.x' },
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' }
   ],
   analyzeCommits: {
     preset: 'angular'
@@ -22,5 +22,6 @@ module.exports = {
     '@semantic-release/github',
     '@semantic-release/npm'
   ],
-  tagFormat: 'prerelease-v${version}'
+  tagFormat: 'prerelease-v${version}',
+  dryRun: true
 };
