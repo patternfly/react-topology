67e4b8ec6f14abbd69333d6342e45788cfb42bf9
Leandro Beretta <lea.beretta@gmail.com>
2023-10-25 09:41:23 -0300

    hulled outline set to true

--- packages/demo-app-ts/src/components/StyleGroup.tsx
+++ packages/demo-app-ts/src/components/StyleGroup.tsx
@@ -84,7 +84,7 @@ const StyleGroup: React.FunctionComponent<StyleGroupProps> = ({
       collapsedWidth={collapsedWidth}
       collapsedHeight={collapsedHeight}
       showLabel={detailsLevel === ScaleDetailsLevel.high}
-      hulledOutline={false}
+      hulledOutline={true}
       {...rest}
       {...passedData}
     >
