0590190f938ca18c71e421c2f51d489b5859c389
Jeffrey Phillips <jephilli@redhat.com>
2024-04-09 07:21:44 -0400

    fix(exports): export pipelineElementFactory and BasePipelineNode

---
+++ packages/module/src/pipelines/elements/index.ts
@@ -0,0 +1,2 @@
+export * from './BasePipelineNode';
+export * from './pipelineElementFactory';
--- packages/module/src/pipelines/index.ts
+++ packages/module/src/pipelines/index.ts
@@ -1,5 +1,6 @@
 export * from './components';
 export * from './decorators';
+export * from './elements';
 export * from './layouts';
 export * from './utils';
 export * from './const';
