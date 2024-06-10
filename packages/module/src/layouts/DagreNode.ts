ce06319767c0fb3026373003c976b5762edbdcdd
Jeffrey Phillips <jephilli@redhat.com>
2024-06-10 07:48:55 -0400

    feat(dagre): Update dagre version to latest release

--- packages/module/src/layouts/DagreNode.ts
+++ packages/module/src/layouts/DagreNode.ts
@@ -1,8 +1,8 @@
+import * as dagre from '@dagrejs/dagre';
 import { LayoutNode } from './LayoutNode';
 export class DagreNode extends LayoutNode implements dagre.Node {
   getUpdatableNode(): dagre.Node {
     return {
-      id: this.id,
       width: this.width,
       height: this.height,
       x: this.x,
