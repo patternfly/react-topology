d4575db2fccc7716b1745e53dfa617225f4aead6
Donald Labaj <donald.labaj@gmail.com>
2024-09-17 16:35:26 -0400

    chore: Updated for migration to main.

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   branches: [
     'do-not-delete',
-    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: 'v6', channel: 'alpha', prerelease: 'alpha' }
+    { name: 'v5', channel: 'prerelease-v5', range: '5.x' },
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' }
   ],
   analyzeCommits: {
     preset: 'angular'
@@ -23,5 +23,5 @@ module.exports = {
     '@semantic-release/npm'
   ],
   tagFormat: 'prerelease-v${version}',
-  dryRun: false
+  dryRun: true
 };
