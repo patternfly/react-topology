4404eb52bc7aa0d54ef1690d9481678a6891644f
Donald Labaj <dlabaj@redhat.com>
2024-12-04 14:13:04 -0500

    fix: Update release.config.js

--- packages/module/release.config.js
+++ packages/module/release.config.js
@@ -2,7 +2,6 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: 'v5', channel: 'prerelease-v5', range: '5.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
