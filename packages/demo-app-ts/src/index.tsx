aad57e0b1ed47765229586407d0ccb3ecbc1c319
Jeff Phillips <jephilli@redhat.com>
2025-06-23 07:37:49 -0400

    chore(demo app): Fix build of demo app and center graph initially (#284)

--- packages/demo-app-ts/src/index.tsx
+++ packages/demo-app-ts/src/index.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import '@patternfly/react-core/dist/styles/base.css';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
 import './index.css';
 import App from './App';
 
